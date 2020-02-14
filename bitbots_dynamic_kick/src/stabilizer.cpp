f7cfe32c7a64e9fd232609ca781c57e4551a19e4
Timon Engelke <timon.engelke@online.de>
2020-02-14 15:56:41 +0100

    dynamic kick: basic setup for more stabilizing methods

--- bitbots_dynamic_kick/src/stabilizer.cpp
+++ bitbots_dynamic_kick/src/stabilizer.cpp
@@ -5,17 +5,134 @@ namespace bitbots_dynamic_kick {
 Stabilizer::Stabilizer() {
   ros::NodeHandle pid_x_nh = ros::NodeHandle("/dynamic_kick/pid_x");
   ros::NodeHandle pid_y_nh = ros::NodeHandle("/dynamic_kick/pid_y");
+  ros::NodeHandle pid_imu_roll_nh = ros::NodeHandle("/dynamic_kick/pid_imu_roll");
+  ros::NodeHandle pid_imu_pitch_nh = ros::NodeHandle("/dynamic_kick/pid_imu_pitch");
+  ros::NodeHandle pid_imu_roll_velocity_nh = ros::NodeHandle("/dynamic_kick/pid_imu_roll_velocity");
+  ros::NodeHandle pid_imu_pitch_velocity_nh = ros::NodeHandle("/dynamic_kick/pid_imu_pitch_velocity");
   pid_x_.init(pid_x_nh, false);
   pid_y_.init(pid_y_nh, false);
+  pid_imu_roll_.init(pid_imu_roll_nh, false);
+  pid_imu_pitch_.init(pid_imu_pitch_nh, false);
+  pid_imu_roll_velocity_.init(pid_imu_roll_velocity_nh, false);
+  pid_imu_pitch_velocity_.init(pid_imu_pitch_velocity_nh, false);
   reset();
 }
 
 void Stabilizer::reset() {
   pid_x_.reset();
   pid_y_.reset();
+  pid_imu_roll_.reset();
+  pid_imu_pitch_.reset();
+  pid_imu_roll_velocity_.reset();
+  pid_imu_pitch_velocity_.reset();
 }
 
 KickPositions Stabilizer::stabilize(const KickPositions &positions, const ros::Duration &dt) {
+  return positions;
+}
+
+bitbots_splines::JointGoals Stabilizer::stabilizeGoals(const bitbots_splines::JointGoals &goals,
+                                                       const ros::Duration &dt, const KickPositions &positions) {
+  return ankleImuVelocity(goals, dt, positions);
+}
+
+bitbots_splines::JointGoals Stabilizer::ankleCop(const bitbots_splines::JointGoals &goals,
+                                                 const ros::Duration &dt,
+                                                 const KickPositions &positions) {
+  return goals;
+}
+
+bitbots_splines::JointGoals Stabilizer::ankleImuOrientation(const bitbots_splines::JointGoals &goals,
+                                                            const ros::Duration &dt,
+                                                            const KickPositions &positions) {
+  //double
+
+  // error is target - state
+  double roll_correction = pid_imu_roll_.computeCommand(imu_roll_, dt);
+  double pitch_correction = pid_imu_pitch_.computeCommand(imu_pitch_, dt);
+
+  bitbots_splines::JointGoals stabilized_goals = goals;
+
+  std::string support_roll_joint, support_pitch_joint;
+  if (positions.is_left_kick) {
+    support_roll_joint = "RAnkleRoll";
+    support_pitch_joint = "RAnklePitch";
+  } else {
+    support_roll_joint = "LAnkleRoll";
+    support_pitch_joint = "LAnklePitch";
+  }
+
+  for (int i = 0; i < stabilized_goals.first.size(); ++i) {
+    if (stabilized_goals.first.at(i) == support_roll_joint) {
+      stabilized_goals.second.at(i) += roll_correction;
+    } else if (stabilized_goals.first.at(i) == support_pitch_joint) {
+      stabilized_goals.second.at(i) += pitch_correction;
+    }
+  }
+
+  return stabilized_goals;
+}
+
+bitbots_splines::JointGoals Stabilizer::ankleImuOrientationFused(const bitbots_splines::JointGoals &goals,
+                                                                 const ros::Duration &dt,
+                                                                 const KickPositions &positions) {
+  return goals;
+}
+
+bitbots_splines::JointGoals Stabilizer::ankleImuVelocity(const bitbots_splines::JointGoals &goals,
+                                                         const ros::Duration &dt,
+                                                         const KickPositions &positions) {
+  // error is target - state, here target is 0
+  double roll_correction = pid_imu_roll_velocity_.computeCommand(0 - imu_roll_velocity_, dt);
+  double pitch_correction = pid_imu_pitch_velocity_.computeCommand(0 - imu_pitch_velocity_, dt);
+
+  bitbots_splines::JointGoals stabilized_goals = goals;
+
+  std::string support_roll_joint, support_pitch_joint;
+  if (positions.is_left_kick) {
+    support_roll_joint = "RAnkleRoll";
+    support_pitch_joint = "RAnklePitch";
+  } else {
+    support_roll_joint = "LAnkleRoll";
+    support_pitch_joint = "LAnklePitch";
+  }
+
+  for (int i = 0; i < stabilized_goals.first.size(); ++i) {
+    if (stabilized_goals.first.at(i) == support_roll_joint) {
+      stabilized_goals.second.at(i) += roll_correction;
+    } else if (stabilized_goals.first.at(i) == support_pitch_joint) {
+      stabilized_goals.second.at(i) += pitch_correction;
+    }
+  }
+
+  return stabilized_goals;
+}
+
+bitbots_splines::JointGoals Stabilizer::hipCop(const bitbots_splines::JointGoals &goals,
+                                               const ros::Duration &dt,
+                                               const KickPositions &positions) {
+  return goals;
+}
+
+bitbots_splines::JointGoals Stabilizer::hipImuOrientation(const bitbots_splines::JointGoals &goals,
+                                                          const ros::Duration &dt,
+                                                          const KickPositions &positions) {
+  return goals;
+}
+
+bitbots_splines::JointGoals Stabilizer::hipImuOrientationFused(const bitbots_splines::JointGoals &goals,
+                                                               const ros::Duration &dt,
+                                                               const KickPositions &positions) {
+  return goals;
+}
+
+bitbots_splines::JointGoals Stabilizer::hipImuVelocity(const bitbots_splines::JointGoals &goals,
+                                                       const ros::Duration &dt,
+                                                       const KickPositions &positions) {
+  return goals;
+}
+
+KickPositions Stabilizer::cartesianCop(const bitbots_dynamic_kick::KickPositions &positions, const ros::Duration &dt) {
   KickPositions stabilized_positions = positions;
   if (positions.cop_support_point && use_cop_) {
     /* calculate stabilizing target from center of pressure
@@ -40,12 +157,35 @@ KickPositions Stabilizer::stabilize(const KickPositions &positions, const ros::D
   return stabilized_positions;
 }
 
+KickPositions Stabilizer::cartesianImuOrientation(const bitbots_dynamic_kick::KickPositions &positions,
+                                                  const ros::Duration &dt) {
+  return positions;
+}
+
+KickPositions Stabilizer::cartesianImuOrientationFused(const bitbots_dynamic_kick::KickPositions &positions,
+                                                       const ros::Duration &dt) {
+  return positions;
+}
+
+KickPositions Stabilizer::cartesianImuVelocity(const bitbots_dynamic_kick::KickPositions &positions,
+                                               const ros::Duration &dt) {
+  return positions;
+
+}
+
 void Stabilizer::useCop(bool use) {
   use_cop_ = use;
 }
 
 void Stabilizer::setRobotModel(moveit::core::RobotModelPtr model) {
   kinematic_model_ = std::move(model);
+}
+
+void Stabilizer::IMUCallback(const sensor_msgs::Imu &imu_msg) {
+  imu_roll_velocity_ = imu_msg.angular_velocity.x;
+  imu_pitch_velocity_ = imu_msg.angular_velocity.y;
+  imu_roll_ = imu_msg.orientation.x;
+  imu_pitch_ = imu_msg.orientation.y;
 }
 
 }
